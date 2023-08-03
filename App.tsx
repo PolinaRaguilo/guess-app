8ed9022672ff61e765dc1c4c4255eb6a2405ffa2
Polina Raguilo <Palina.Rahila@ventionteams.com>
2023-08-03 17:48:25 +0300

    init project

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hi appshh!</Text>
+        <Button title="test" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
